25e586cb8a17f13e7bd6815142918b2f54c4e500
Magesh J <magesh.lbj23@gmail.com>
2023-01-24 18:36:08 +0530

    initial state set to empty

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -1,32 +1,6 @@
 export const initialState = {
 	habits: {
-		greens: [
-			{
-				id: 1,
-				name: "Hit the gym",
-				done: false,
-			},
-			{
-				id: 2,
-				name: "Read 10 pages",
-				done: false,
-			},
-		],
-		reds: [
-			{
-				id: 1,
-				name: "No Deep fry",
-				done: false,
-				entries: [],
-				entryTimes: [],
-			},
-			{
-				id: 2,
-				name: "No toxicity",
-				done: false,
-				entries: [],
-				entryTimes: [],
-			},
-		],
+		greens: [],
+		reds: [],
 	},
 };
