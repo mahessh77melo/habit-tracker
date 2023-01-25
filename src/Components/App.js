78e80c10d8b5e580b271002b9311bdc566b07600
Magesh J <magesh.lbj23@gmail.com>
2023-01-25 11:05:56 +0530

    media queries

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,43 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react";
 import "../scss/App.scss";
 import GreenColumn from "./GreenColumn";
 import RedColumn from "./RedColumn";
 import TitleColumn from "./TitleColumn";
 
 function App() {
+	const [isNarrowScreen, setIsNarrowScreen] = useState(false);
+
+	useEffect(() => {
+		// set initial value
+		const mediaWatcher = window.matchMedia("(max-width: 900px)");
+		setIsNarrowScreen(mediaWatcher.matches);
+		//watch for updates
+		function updateIsNarrowScreen(e) {
+			setIsNarrowScreen(e.matches);
+		}
+		mediaWatcher.addEventListener("change", updateIsNarrowScreen);
+
+		// clean up after ourselves
+		return function cleanup() {
+			mediaWatcher.removeEventListener("change", updateIsNarrowScreen);
+		};
+	});
 	return (
 		<div className="app">
-			<GreenColumn />
-			<TitleColumn />
-			<RedColumn />
+			{!isNarrowScreen ? (
+				<>
+					<GreenColumn />
+					<TitleColumn />
+					<RedColumn />
+				</>
+			) : (
+				<>
+					<TitleColumn />
+					<GreenColumn />
+					<RedColumn />
+				</>
+			)}
 		</div>
 	);
 }
